e39fadb8ebf6dda0349a5ae456406b55107f4803
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-04 15:31:38 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,33 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  5.times { puts string}
+  
+end
+
+
+def loop_message_n_times(string,integer)
+  
+  integer.times {puts string}
+end
+
+def output_array(array)
+  
+  array.each { |i| puts array}
+  
+end
+
+def return_string_array(array)
+  array.map {|x|  x.to_s }
+end
+
+
+
+
+
+
+
+
+
+
+
